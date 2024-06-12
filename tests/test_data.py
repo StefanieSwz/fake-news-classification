4ac2ac7fa6982c082f5e8c1a8f81879371959120
annegriddl <anne.gritto@campus.lmu.de>
2024-06-12 15:08:10 +0200

    changes in test_data and start with test_model

--- tests/test_data.py
+++ tests/test_data.py
@@ -9,6 +9,7 @@ import shutil
 import zipfile
 from fakenews.data.preprocessing import DataPreprocessor
 from fakenews.data.make_dataset import download_dataset, unzip_dataset
+from fakenews.config import RAW_DATA_DIR, PROCESSED_DATA_DIR
 
 @pytest.fixture
 def temp_dir():
@@ -21,28 +22,36 @@ def data_preprocessor(temp_dir):
     preprocessor = DataPreprocessor(temp_dir, max_length=128)
     data = pd.DataFrame({
         "title": ["Fake news title 1", "Fake news title 2"],
+        "text": ["Fake news text 1", "Fake news text 2"],
         "label": [1, 0]
     })
     data.to_csv(os.path.join(temp_dir, "WELFake_Dataset.csv"), index=False)
     return preprocessor
 
-def test_load_data(data_preprocessor):
+def test_load_data():
+    data_preprocessor = DataPreprocessor(RAW_DATA_DIR, max_length=128)
     data = data_preprocessor.load_data()
     assert isinstance(data, pd.DataFrame)
     assert not data.empty
 
+def test_data_shape():
+    data_preprocessor = DataPreprocessor(RAW_DATA_DIR, max_length=128)
+    data = data_preprocessor.load_data()
+    assert data.shape[0] == 72134
+    assert "title" in data.columns
+    assert "label" in data.columns
+    assert "text" in data.columns
+
 def test_preprocess_data(data_preprocessor):
-    data = pd.DataFrame({
-        "title": ["Fake news title 1", "Fake news title 2"],
-        "label": [1, 0]
-    })
-    preprocessed_data = data_preprocessor.preprocess_data(data)
-    assert "label" in preprocessed_data.columns
-    assert pd.api.types.is_integer_dtype(preprocessed_data["label"])
+    data_preprocessor = DataPreprocessor(RAW_DATA_DIR, max_length=128)
+    data = data_preprocessor.load_data()
+    data = data_preprocessor.preprocess_data(data)
+    assert pd.api.types.is_integer_dtype(data["label"])
 
 def test_save_preprocessed_data(data_preprocessor, temp_dir):
     data = pd.DataFrame({
         "title": ["Fake news title 1", "Fake news title 2"],
+        "text": ["Fake news text 1", "Fake news text 2"],
         "label": [1, 0]
     })
     preprocessed_data = data_preprocessor.preprocess_data(data)
@@ -50,24 +59,21 @@ def test_save_preprocessed_data(data_preprocessor, temp_dir):
     saved_file_path = os.path.join(temp_dir, "preprocessed_data.csv")
     assert os.path.exists(saved_file_path)
 
-def test_load_preprocessed_data(data_preprocessor, temp_dir):
-    data = pd.DataFrame({
-        "title": ["Fake news title 1", "Fake news title 2"],
-        "label": [1, 0]
-    })
-    preprocessed_data = data_preprocessor.preprocess_data(data)
-    data_preprocessor.save_preprocessed_data(preprocessed_data, temp_dir)
-    loaded_data = data_preprocessor.load_preprocessed_data(temp_dir)
+def test_load_preprocessed_data():
+    data_preprocessor = DataPreprocessor(RAW_DATA_DIR, max_length=128)
+    loaded_data = data_preprocessor.load_preprocessed_data(PROCESSED_DATA_DIR)
     assert isinstance(loaded_data, pd.DataFrame)
     assert not loaded_data.empty
 
 @patch('subprocess.run')
+# @patch temporarily replaces the specified object with a Mock object; after the test, the original object is restored
 def test_download_dataset(mock_run, temp_dir):
     dataset_name = "someuser/somedataset"
     download_dataset(dataset_name, temp_dir)
     dataset_zip_path = os.path.join(temp_dir, f"{dataset_name.split('/')[1]}.zip")
     mock_run.assert_called_with(["kaggle", "datasets", "download", "-d", dataset_name, "-p", temp_dir])
     assert os.path.exists(temp_dir)
+
 
 @patch('zipfile.ZipFile.extractall')
 def test_unzip_dataset(mock_extractall, temp_dir):
---
+++ tests/test_model.py
@@ -0,0 +1,32 @@
+"""This script tests the models."""
+import os
+import pytest
+import pytest
+import torch
+from omegaconf import OmegaConf, DictConfig
+from transformers import AutoTokenizer
+from torch.utils.data import DataLoader, TensorDataset
+from fakenews.models.model import BERTClass
+from hydra import initialize, compose
+import pytorch_lightning as pl
+from transformers import AutoModel
+
+# Load the configuration from the YAML file
+#cfg = OmegaConf.load('config/model/model.yaml')
+
+# Initialize Hydra and load the configuration
+@pytest.fixture
+def cfg():
+    with initialize(config_path="../config"):
+        cfg = compose(config_name="config")
+    return cfg
+
+@pytest.fixture
+def model(cfg):
+    return BERTClass(cfg)
+
+
+def test_model_initialization(cfg):
+    model = BERTClass(cfg)
+    assert model.dropout.p == cfg.model.dropout_rate
+
